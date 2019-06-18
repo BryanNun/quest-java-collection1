5834815283910989c1d27b48f2278481dc42d54a
BryanNun <nuninger.bryan@gmail.com>
2019-06-18 17:46:03 +0200

    resultat de l'exercice List

--- Hero.java
+++ Hero.java
@@ -1,8 +1,8 @@
 public class Hero {
 
-    // attributs
-    private String name;
-    private int age;
+        // attributs
+        private String name;
+        private int age;
 
     // constructeurs
     public Hero(String name, int age) {
