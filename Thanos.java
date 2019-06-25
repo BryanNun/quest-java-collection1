a80fdb4db3bdeda60c8c118dd7b33c792b052b1a
BryanNun <nuninger.bryan@gmail.com>
2019-06-25 17:35:33 +0200

    correction de l'exercice

--- Thanos.java
+++ Thanos.java
@@ -5,30 +5,19 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-        ArrayList<Hero> lunch = new ArrayList<>();
+        ArrayList<Heroes> lunch = new ArrayList<>();
 
-        Hero black = new Hero ("Black Widow", 34);
-        Hero captain = new Hero ("Captain America",100);
-        Hero vision = new Hero ("Vision", 3);
-        Hero iron = new Hero ("Iron Man",48);
-        Hero scarlet = new Hero ("Scarlet Witch",29);
-        Hero thor = new Hero ("Thor",1500);
-        Hero spider = new Hero ("Spider-Man",18);
-        Hero hulk = new Hero ("Hulk",49);
-        Hero doctor = new Hero ("Doctor Strange",42);
+        heroes.add(new Hero("Black Widow", 34));
+        heroes.add(new Hero("Captain America",100));
+        heroes.add(new Hero("Vision", 3));
+        heroes.add(new Hero("Iron Man",48));
+        heroes.add(new Hero("Scarlet Witch",29));
+        heroes.add(new Hero("Thor",1500));
+        heroes.add(new Hero("Spider-Man",18));
+        heroes.add(new Hero("Hulk",49));
+        heroes.add(new Hero("Doctor Strange",42));
 
-
-        lunch.add(black);
-        lunch.add(captain);
-        lunch.add(vision);
-        lunch.add(iron);
-        lunch.add(scarlet);
-        lunch.add(thor);
-        lunch.add(spider);
-        lunch.add(hulk);
-        lunch.add(doctor);
-
-        thor.setAge(1501);
+        heroes.get(5).setAge(1501);
         
         Collections.shuffle(lunch);
 
@@ -39,6 +28,6 @@ public class Thanos {
         }
         // TODO 5 : Keep only the half of the list
 
-        // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        // TODO 6 : Loop throught the list and display the name of the remaining Heroeses
     }
 }
